bd2560a8a86cb50919cb58b0d8030a0600a2c113
Alexi_Dg <alexisdurangomez588@gmail.com>
2025-12-10 20:25:37 -0500

    ":poop: unknown"

--- index.js
+++ index.js
@@ -1,54 +1,11 @@
-function Welcome(dates) {
-    console.log("<!--Welcome 2-->")
-    console.table(dates);
-    if (dates.Nombre) {
-        console.log(dates.Nombre);
-    } else {
-        console.log('Faltan datos')
-    }
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-    if (dates.Edad) {
-        console.log(dates.Edad);
-    } else {
-        console.log('Faltan datos')
-    }
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
 
-    if (dates.Ciudad) {
-        console.log(dates.Ciudad)
-    } else {
-        console.log('No hay datos');
-    }; 
-}
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
 
-function Welcome2(nombre, edad, ciudad) {
-    console.log("<!--Welcome 2-->")
-    if (nombre) {
-        console.log(nombre);
-    } else  {
-        console.log('Faltan datos W2')
-    }
-    if (edad) {
-        console.log(edad);
-    } else  {
-        console.log('Faltan datos W2')
-    }
-    if (ciudad) {
-        console.log(ciudad);
-    } else {
-        console.log('No hay datos');
-    } 
-};
-
-let John = {
-    Nombre: "John",
-    Edad: 30,
-    Ciudad: "Bogota"
-};
-
-Welcome({
-    Nombre: "Alexi",
-    Edad: 20,
-    Ciudad: "Bucaramanga"
-});
-
-Welcome2(John.Nombre, John.Edad, John.Ciudad);
+let guessCount = 1;
+let resetButton;
